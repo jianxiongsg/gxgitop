68291328e4a17efaa04fe37a59dd748a0716e3a4
jianxiong <jianxiong@lovigame.com>
2020-01-05 08:12:29 +0800

    up

---
+++ README.md
@@ -0,0 +1,69 @@
+# jxgitop README
+
+这是您的扩展名“jxgitop”的自述文件。在写了一个简短的描述之后，我们建议包括以下部分。
+
+## Features
+
+请描述扩展的特定功能，包括正在运行的扩展的屏幕截图。图像路径与此自述文件相关。
+
+
+
+For example if there is an image subfolder under your extension project workspace:
+
+\!\[feature X\]\(images/feature-x.png\)
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Requirements
+
+如果您有任何需求或依赖项，请添加一节来描述这些需求和如何安装和配置它们。
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
+
+...........................
+
+-----------------------------------------------------------------------------------------------------------
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as spawn from 'cross-spawn';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "jxgitop" is now active!');
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.jxgitop', () => {
+		// The code you place here will be executed every time your command is executed
+		vscode.workspace.onDidChangeTextDocument(event=>{
+			if(event.document){
+				console.log('...... event.document.uri: ',event.document.uri)
+			}
+		})
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Hello World!!!');
+		// let cwd = vscode.workspace.rootPath;
+		// if(!cwd){
+		// 	return;
+		// }
+		// console.log('context.extensionPath',context.extensionPath)
+		// console.log('context.logPath',context.logPath)
+		// console.log('context.context',context)
+		// console.log('context.asRelativePath',vscode.workspace.asRelativePath)
+		// console.log('context.workspaceFolders',vscode.workspace.workspaceFolders)
+		// console.log('......rootPath cwd: ',vscode.workspace.rootPath)
+		// spawn('jxgitop',['-d',cwd],{
+        //     cwd:cwd,
+        //     // shell:true
+        // })
+	});
+
+	context.subscriptions.push(disposable);
+	const key = 'vscodePluginDemo.jxgitop';
+    if (vscode.workspace.getConfiguration().get(key)) {
+        vscode.commands.executeCommand('extension.jxgitop');
+    }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
